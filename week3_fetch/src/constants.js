14cf956fa84181508683c72be1ac5a6fe53d74b8
Owall <97934878+MayOwall@users.noreply.github.com>
2022-04-07 14:22:07 +0900

    week3_fetch/ constants.js 삭제 및 api.js 생성
    필요없는 constant.js를 삭제하였습니다.
    기본적인 request동작을 수행할 api.js를 생성했습니다.

--- week3_fetch/src/constants.js
+++
@@ -1,9 +0,0 @@
-const PRODUCT_ID_URL = "https://kdt.roto.codes/products/:id";
-const PRODUCT_OPTION_URL = "https://kdt.roto.codes/product-options?product.id=:productId";
-const PRODUCT_OPTION_STOCK_URL = "https://kdt.roto.codes/product-option-stocks?productOption.id=:productOptionId"
-
-export default {
-  PRODUCT_ID_URL,
-  PRODUCT_OPTION_URL,
-  PRODUCT_OPTION_STOCK_URL
-};
