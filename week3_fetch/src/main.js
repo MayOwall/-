cd1cac56fbb21dc9ff78df74a8899cfbc2a9a325
Owall <97934878+MayOwall@users.noreply.github.com>
2022-04-07 16:08:42 +0900

    week3_fetch/ main, api 수정
    main과 api를 수정하여 main에서 외부 API를 받아와 랜더링 할 수 있도록 하였습니다.

--- week3_fetch/src/main.js
+++ week3_fetch/src/main.js
@@ -1,4 +1,5 @@
 import ProductOptions from "./ProductOption.js";
+import { request } from "./api.js";
 
 const dummyData = [
   {
@@ -22,10 +23,41 @@ const dummyData = [
 ];
 
 const root = document.querySelector("#root");
+const DEFAULT_PRODUCT_ID = 1;
+const FetchOptionData = (productId) => {
+  return request(`/products/${productId}`)
+    .then(product => {
+      return request(`/product-options?product.id=${product.id}`);
+    })
+    .then(productOptions => {
+      return Promise.all([
+        Promise.resolve(productOptions),
+        Promise.all(
+          productOptions.map(productOption => productOption.id).map(id => {
+            return request(`/product-option-stocks?productOption.id=${id}`)
+          })
+        )
+      ])
+    })
+    .then(data => {
+      const [ productOptions, stocks ] = data;
+      const optionData = productOptions.map(( productOption, i ) => {
+        const stock = stocks[i][0].stock
+        return {
+          optionId : productOption.id,
+          optionName : productOption.optionName,
+          optionPrice : productOption.optionPrice,
+          stock
+        }
+      });
+      productOptionsComponent.setState(optionData)
+    })
+};
+FetchOptionData(DEFAULT_PRODUCT_ID);
 
-new ProductOptions({
+const productOptionsComponent = new ProductOptions({
   target : root,
-  initialState : dummyData,
+  initialState : [],
   onSelect : (option) => {
     alert(`${option.optionName}. 현재 재고는 ${option.stock}개 이소이다.`);
   }
